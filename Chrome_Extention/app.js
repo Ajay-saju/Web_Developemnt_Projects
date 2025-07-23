068f94ca7f49894bba65a95b03143e2e6b492da1
unknown <ajaysaju987@gmail.com>
2025-07-23 13:12:47 +0530

    extension complete

--- Chrome_Extention/app.js
+++ Chrome_Extention/app.js
@@ -1,13 +1,36 @@
-// function saveLead(){
-//     console.log("Log out button clicked ");
-// }
 
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+let myLeads = [];
 
-inputBtn.addEventListener("click",function(){
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
+const saveButton = document.querySelector("#input-btn")
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
+console.log(ulEl);
+
+
+saveButton.addEventListener('click', function () {
+
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+    renderLeads();
+
+    
 })
+
+let count = 0;
+
+function renderLeads() {
+    for (let i = count; i< myLeads.length; i++) {
+
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+        a.href = myLeads[i];
+        a.textContent = myLeads[i];
+        a.target = "-blank"
+        li.appendChild(a);
+        ulEl.appendChild(li)
+        count++;
+
+    }
+}
+
