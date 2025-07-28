56b1d2e5d5b199a17b98df32c4be6af95298b189
unknown <ajaysaju987@gmail.com>
2025-07-28 18:34:39 +0530

    current tab save comepleted

--- Chrome_Extention/app.js
+++ Chrome_Extention/app.js
@@ -1,46 +1,39 @@
 
 // localStorage.clear();
 
-let myLeads = [];  // place to save input leads from input fields 
-
-
-let savedLeads = [];
-
+let myLeads = [];
 let count = 0;
 
 const saveButton = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
+const deleteBtn = document.querySelector("#delete-btn")
+const saveTabBtn = document.querySelector("#saveTab-btn")
 
 
 if (localStorage.getItem("saveLeads")) {
-    showSavedLeads() 
+    showSavedLeads()
 }
+
+const tab = [{ url: "www.google.com" }]
+
+saveTabBtn.addEventListener("click", function () {
+
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.getItem("myleads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    });
+
+
+})
 
 function showSavedLeads() {
-    console.log("show saved leads working")
+
     const savedLeadsJson = localStorage.getItem("saveLeads");
-    savedLeads = JSON.parse(savedLeadsJson);
-
-    myLeads = savedLeads
-    renderLeads()
-
-}
-
-
-function renderLeads() {
-
-    for (let i = count; i < myLeads.length; i++) {
-
-        const li = document.createElement("li")
-        const a = document.createElement("a")
-        a.href = myLeads[i];
-        a.textContent = myLeads[i];
-        a.target = "-blank"
-        li.appendChild(a);
-        ulEl.appendChild(li)
-        count++;
-    }
+    myLeads = JSON.parse(savedLeadsJson);
+    renderLeads(myLeads)
 
 }
 
@@ -52,10 +45,41 @@ saveButton.addEventListener('click', function () {
     myLeads.push(inputEl.value);
     inputEl.value = ""
     localStorage.setItem("saveLeads", JSON.stringify(myLeads))
-    renderLeads();
+    renderLeads(myLeads);
 
 
 })
+
+deleteBtn.addEventListener("dblclick", function () {
+
+    console.log("delete btn working ")
+
+    localStorage.clear()
+    myLeads = []
+
+
+    renderLeads(myLeads)
+
+})
+
+
+function renderLeads(leads) {
+    ulEl.innerHTML = "";
+    for (let i = 0; i < leads.length; i++) {
+
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+        a.href = leads[i];
+        a.textContent = leads[i];
+        a.target = "_blank"
+        li.appendChild(a);
+        ulEl.appendChild(li)
+        count++;
+    }
+
+}
+
+
 
 
 
